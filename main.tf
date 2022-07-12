06a64c4e5cb3bb1dcdd9f22161a93b52a4f3ccec
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-07-11 22:17:24 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudformation_slack" "network" {
+  name = "networking-stack"
+  
+  parameteners = {}
+  capabilities = ["CAPABILITY_IAM"]
+  
+  template_body = <<STACK
+{
+    "Parameters": {},
+    "Resources": [
+      "LB14H9U": {
+        "Type": "AWS::Lex::Bot",
+        "Properties": {
+          "Name": "MegamindFromTerraform",
+          "RoleArn":"arn:aws:iam::077492956248:role/lambda-invoke-role",
+          "DataPrivacy": {
+            "ChildDirected": false
+          },
+          "idleSessionTTLInSeconds": 300,
+          "Description": "Basic bot created from terraform"
+        }
+      }
+    }
+  }
+  STACK
+}
