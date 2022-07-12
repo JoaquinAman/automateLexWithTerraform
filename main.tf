4debc59ceec62e8315197884a1c49448338d9b42
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-07-12 09:25:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,44 @@
+# resource "aws_cloudformation_stack" "network2" {
+#     name = "networking-stack2"
+    
+#     parameters = {}
+#     capabilities = ["CAPABILITY_IAM"]
+    
+#     template_body = <<STACK
+#   {
+#       "Parameters": {},
+#       "Resources": {
+#         "LB14H9U": {
+#           "Type": "AWS::Lex::Bot",
+#           "Properties": {
+#             "Name": "MegamindFromTerraform",
+#             "RoleArn":"arn:aws:iam::077492956248:role/lambda-invoke-role",
+#             "DataPrivacy": {
+#               "ChildDirected": false
+#             },
+#             "IdleSessionTTLInSeconds": 300,
+#             "Description": "Basic bot created from terraform"
+#           }
+#         }
+#       }
+#     }
+#     STACK
+#   }
+
 resource "aws_cloudformation_stack" "network2" {
     name = "networking-stack2"
-    
+
     parameters = {}
     capabilities = ["CAPABILITY_IAM"]
-    
+
     template_body = <<STACK
+    # Constructing AWS Lex bot, adding intent and slot types:
+    # https://docs.aws.amazon.com/lex/latest/dg/lambda-build-intents-slots.html
+    # https://docs.aws.amazon.com/lex/latest/dg/lambda-build-intents-slots.html#lambda-build-intents-slots-lex-bot
+
+    # Constructing AWS Lex bot, adding intent and slot types:
+
+   
   {
       "Parameters": {},
       "Resources": {
@@ -18,9 +52,50 @@ resource "aws_cloudformation_stack" "network2" {
             },
             "IdleSessionTTLInSeconds": 300,
             "Description": "Basic bot created from terraform"
+
+
           }
-        }
+
+
+        },
+        "LB14H9UIntent": {
+          "Type": "AWS::Lex::Intent",
+          "Properties": {
+            "Name": "MegamindFromTerraform",
+            "BotName": "MegamindFromTerraform",
+            "IntentVersions": [
+              {
+                "Name": "MegamindFromTerraform",
+                "Description": "Basic bot created from terraform"
+              }
+            ]
+          }
+        },
+        "LB14H9USlotType": {
+          "Type": "AWS::Lex::SlotType",
+          "Properties": {
+            "Name": "MegamindFromTerraform",
+            "Description": "Basic bot created from terraform",
+            "EnumerationValues": [
+              {
+                "Value": "Megamind",
+                "Synonyms": [
+                  "Megamind"
+                ]
+              },
+              {
+                "Value": "Megamind2",
+                "Synonyms": [
+                  "Megamind2"
+                ]
+              }
+            ]
+          }
+        },
+
+
       }
+
     }
     STACK
   }
